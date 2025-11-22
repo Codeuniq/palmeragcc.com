8fd79acf29029ec3946682c39c6dca6a1a840847
Faisan333 <azeezfaizan@gmail,com>
2025-11-22 13:37:51 -0800

    Change

--- js/custom.js
+++ js/custom.js
@@ -156,29 +156,6 @@ autoSlidingDelay - delay in ms. If audoSliding is on and no value provided, defa
 blockASafterClick - boolean. If user clicked any sliding control, autosliding won't start again
 */
 
-fncSlider(".example-slider", {
-  autoSlidingDelay: 4000
-});
-var $demoCont = document.querySelector(".demo-cont");
-[].slice
-  .call(document.querySelectorAll(".fnc-slide__action-btn"))
-  .forEach(function($btn) {
-    $btn.addEventListener("click", function() {
-      $demoCont.classList.toggle("credits-active");
-    });
-  });
-document
-  .querySelector(".demo-cont__credits-close")
-  .addEventListener("click", function() {
-    $demoCont.classList.remove("credits-active");
-  });
-document
-  .querySelector(".js-activate-global-blending")
-  .addEventListener("click", function() {
-    document
-      .querySelector(".example-slider")
-      .classList.toggle("m--global-blending-active");
-  });
 
 /* 1. Proloder */
     $(window).on('load', function () {
